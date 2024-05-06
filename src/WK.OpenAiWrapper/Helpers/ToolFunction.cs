73ebe6e3bbba7a9c4589566cb1043410585865ad
Stefan Bechtel <SBechtel@werbas.com>
2024-05-06 10:50:22 +0200

    fix(ToolFunctions): Fixed bug preventing multiple use of functions in Pilot tools Resolved a bug that was preventing functions in the Pilot tools from being used more than once. Now, a check is performed to see if the function already exists, and if so, it is reused. Previously, this scenario would throw an exception.

--- src/WK.OpenAiWrapper/Helpers/ToolFunction.cs
+++ src/WK.OpenAiWrapper/Helpers/ToolFunction.cs
@@ -7,15 +7,14 @@ public record ToolFunction(string MethodFullName, string? Description = null)
 {
     public Tool GenerateTool()
     {
-        ConstructorInfo constructorInfo = typeof(Function)
-            .GetConstructors(BindingFlags.NonPublic | BindingFlags.Instance)
-            .Single(c => c.GetParameters().Length > 3);
+        MethodInfo methodInfoGetOrCreateFunction = typeof(Function)
+            .GetMethod("GetOrCreateFunction", BindingFlags.NonPublic | BindingFlags.Static);
         ToolFunctionInfo toolFunctionInfo = new (MethodFullName);
         MethodInfo methodInfo = toolFunctionInfo.GetMethodInfo();
         object instance = methodInfo.IsStatic ? null : Activator.CreateInstance(methodInfo.DeclaringType);
             
         //(string name, string description, MethodInfo method, object instance = null)
-        Function function = (Function)constructorInfo.Invoke(new []{ toolFunctionInfo.MethodName, Description ?? toolFunctionInfo.Description, methodInfo, instance });
+        Function function = (Function)methodInfoGetOrCreateFunction.Invoke(null, [toolFunctionInfo.MethodName, Description ?? toolFunctionInfo.Description, methodInfo, instance]);
         return new Tool(function);
     }
 }
