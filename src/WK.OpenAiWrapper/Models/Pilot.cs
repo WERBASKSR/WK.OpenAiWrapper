e50b77af82477edb0d20748041ee85f006eac0b6
Stefan Bechtel <SBechtel@werbas.com>
2024-06-03 11:14:45 +0200

    fix(PilotNames and Tests): Updated pilot name uniqueness check and corrected test Temporarily disabled the use of `gpt-4o` pending an update to the 'OpenAI-DotNet' NuGet package. Fixed a bug where the uniqueness of pilot names was checked at the correct place, i.e., after merging pilots from appsettings and manually added ones. Also corrected a faulty test.

--- src/WK.OpenAiWrapper/Models/Pilot.cs
+++ src/WK.OpenAiWrapper/Models/Pilot.cs
@@ -4,10 +4,26 @@ using WK.OpenAiWrapper.Helpers;
 
 namespace WK.OpenAiWrapper.Models;
 
-public record Pilot(string Name, string Instructions, string Description, string Model = "gpt-4o")
+public record Pilot(string Name, string Instructions, string Description)
 {
-    // ReSharper disable once CollectionNeverUpdated.Global
-    // ReSharper disable once ReturnTypeCanBeEnumerable.Global
+    private string? _model = "gpt-4o";
+
+    public string? Model
+    {
+        get
+        {
+            //Bugfix until the nuget 'OpenAI-DotNet' update is available and the gpt-4o can be used
+            //Todo: Delete this condition again as soon as the nuget update is available
+            if (_model == "gpt-4o")
+            {
+                return "gpt-4-turbo";
+            }
+
+            return _model;
+        }
+        set => _model = value;
+    }
+
     public ICollection<Tool> Tools { get; } = new HashSet<Tool>();
     public ICollection<ToolFunction> ToolFunctions { get; } = new HashSet<ToolFunction>();
 
